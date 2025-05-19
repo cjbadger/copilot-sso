7888e0f01cd716935e7fba83bf8936dc3bf3eb51
Christopher Barnaby @ Barhead <christopher.barnaby@barhead.com>
2025-05-19 14:47:37 +1000

    missing endpoint

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log("Hello, World!");
+
+var clientApplication;
+    (function () {
+    var msalConfig = {
+        auth: {
+            clientId: '<Client ID [CanvasClientId]>',
+            authority: 'https://login.microsoftonline.com/<Directory ID>'
+        },
+        cache: {
+            cacheLocation: 'localStorage',
+            storeAuthStateInCookie: false
+        }
+    };
+    if (!clientApplication) {
+        clientApplication = new Msal.UserAgentApplication(msalConfig);
+    }
+    } ());
